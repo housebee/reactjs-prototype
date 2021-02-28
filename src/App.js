72ea7fed8dfc41e42eccf76cc48eb17a30250c60
housebee <shaopeng.sun@gmail.com>
2021-02-28 12:26:33 -0600

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import MyButton from './MyButton';
 import "./style.css";
 
 export default function App() {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+
+  const increaseTemperature = () => {
+    setTemperatureValue(temperatureValue + 1);
+  };
+
+  const decreaseTemperature = () => {
+    setTemperatureValue(temperatureValue - 1);
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div class="app-container">
+      <div class="temperature-display-container">
+        <div class="temperature-display">{temperatureValue}°C</div>
+      </div>
+      <div class="button-container">
+        <MyButton clickHandler={increaseTemperature} label="+" />
+        <MyButton clickHandler={decreaseTemperature} label="-" />
+      </div>
     </div>
   );
 }
+
+
