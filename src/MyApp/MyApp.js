1f06a133cb7430963a36cbd63c016aa128607a82
housebee <shaopeng.sun@gmail.com>
2021-02-28 20:31:21 -0600

    1 file modified, 2 added

---
+++ src/MyApp/MyApp.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./MyApp.css";
+
+class MyApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>My App...</h3>
+      </div>
+    );
+  }
+}
+
+export default MyApp;
--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+//import MyApp from "./MyApp/MyApp";
+//ReactDOM.render(<MyApp />, document.getElementById("root"));
